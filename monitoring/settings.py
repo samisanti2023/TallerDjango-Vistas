851ce407d422b3761b22b2ac2bf89cb7f2fee9a4
Samuel Pena <ss.pena@uniandes.edu.co>
2025-02-03 17:01:56 -0500

    hola

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "hola",
+        "USER": "samis",
+        "PASSWORD": "samis",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
